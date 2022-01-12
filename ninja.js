5c29dee5cf9ec1173b5b8ebe0d94dbdadbebd85c
Andrew <aschoch10@gmail.com>
2022-01-11 18:07:00 -0800

    super_ninja

--- ninja.js
+++ ninja.js
@@ -7,7 +7,7 @@ class Ninja{
     }
 
     sayName(){
-        console.log(`${this.name}`)
+        console.log(this.name)
         return this;
     }
 
@@ -25,7 +25,28 @@ class Ninja{
     }
 }
 
+class Sensei extends Ninja {
+    constructor(name, health=200, speed=20, strength=20, wisdom = 10){
+
+        super(name, health, speed , strength)
+        this.speed = speed;
+        this.health = health;
+        this.strength = strength;
+        this.wisdom = wisdom;
+    }
+
+    speakWisdom(){
+        this.drinkRainier();
+        console.log(`${this.name} is the smartest Sensei in the world!`)
+    }
+
+}
+
 const ninja1 = new Ninja("James", 100)
-console.log(ninja1)
-ninja1.sayName().showStats().drinkRainier()
-ninja1.showStats()
+// console.log(ninja1)
+// ninja1.sayName().showStats().drinkRainier()
+// ninja1.showStats()
+
+const masterSensei = new Sensei("Johnny", 400)
+console.log(masterSensei)
+masterSensei.speakWisdom()
